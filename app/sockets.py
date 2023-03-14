97e8cb01fe8ed1f8def53790d09a964ba37410dc
enkhzayaqt <jini.zaya@gmail.com>
2023-03-13 21:12:16 -0500

    heroku

--- app/sockets.py
+++ app/sockets.py
@@ -14,8 +14,7 @@ if os.environ.get("FLASK_ENV") == "production":
         "http://group-4-discord.onrender.com",
         "http://discord-clone-maks.onrender.com",
         "http://discordance.onrender.com",
-        "http://discord-clone-31dg.onrender.com",
-        "https://discordance-group-project-2qx9.onrender.com"
+        "https://discordance-group.herokuapp.com/"
         ]
 else:
     origins = "*"
